7d32d578df2fa9fc4759d5a18cca4fcfd28c1ad1
fuwadWAS <mohibulhasanfuwad@gamil.com>
2023-10-03 20:40:09 +0600

    practice-3

---
+++ js/tailwind.config.js
@@ -0,0 +1,23 @@
+
+
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          header: 'hsla(31, 100%, 63%, 0.694)',
+        },
+        fontFamily: {
+            open: ["Open Sans", 'sans-serif'],
+            
+          },
+          backgroundImage: {
+            banner: "url('./images/banner.png')",
+            pic: "url('./images/banner1.jpg')",
+            bannertwo: "url('./images/banner2.jpg')",
+            
+          }
+        }
+      }
+    }
+    
+  
